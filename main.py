d5b8a9162bc0e7d76ae14218ac1d4c2240746c1e
Vitah <vumirek@gmail.com>
2023-04-04 03:14:11 +0200

    count and finish

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import re
 
 def clean(text: str):
     text = text.lower()
-    return re.sub(r'[^a-z]', '', text)
+    return re.sub(r"[^a-z]", "", text)
 
 
 def normalize_vector(vector: np.array) -> np.array:
@@ -43,24 +43,71 @@ def perceptron(
     theta: float,
     input_vector: np.array,
 ) -> float:
-    return (
-        1 if np.sum(weight_vector * input_vector) >= theta else 0
+    return 1 if np.sum(weight_vector * input_vector) >= theta else 0
+
+
+def train_layer(row, classes, theta_mapper, theta):
+    input_vector = create_char_count_list(row[1])
+    input_vector = normalize_vector(input_vector)
+    d = np.array([1 if row[0] == key else 0 for key in classes.keys()])
+    y = np.array(
+        [
+            perceptron(classes[key], theta[theta_mapper[key]], input_vector)
+            for key in classes.keys()
+        ]
     )
+    return d, y, input_vector
+
+
+def modify_params(classes, theta_mapper, theta, alpha, d, y, input_vector):
+    for key in classes.keys():
+        classes[key] = modify_weight_vector(
+            classes[key],
+            alpha,
+            d[theta_mapper[key]],
+            y[theta_mapper[key]],
+            input_vector,
+        )
+    theta = np.array(
+        [modify_theta(theta[i], alpha, d[i], y[i]) for i in range(len(classes))]
+    )
+    return classes, theta
+
+
+def test_layer(i, classes, theta_mapper, theta, test_data, data, alpha):
+    correct = 0
+    for r in test_data:
+        input_vector = create_char_count_list(r[1])
+        input_vector = normalize_vector(input_vector)
+        y = np.array(
+            [
+                perceptron(classes[key], theta[theta_mapper[key]], input_vector)
+                for key in classes.keys()
+            ]
+        )
+        if r[0] == list(classes.keys())[np.argmax(y)]:
+            correct += 1
+    with open(f"results_{alpha}.csv", "a") as f:
+        f.write(f"{i};{correct / len(test_data) * 100}\n")
 
 
 def main():
-    reader = csv.reader(open('lang.train.csv', 'r', encoding='utf-8'))
+    reader = csv.reader(open("lang.train.csv", "r", encoding="utf-8"))
     data = np.array([[row[0], clean(row[1])] for row in reader])
-    classes = {row: np.random.random_sample(26) for row in np.unique(data[:, 0])}
-    theta_mapper = {list(classes.keys())[i]: i for i in range(len(classes))}
+    test_reader = csv.reader(open("lang.test.csv", "r", encoding="utf-8"))
+    test_data = np.array([[row[0], clean(row[1])] for row in test_reader])
     alpha = 0.1
-    theta = np.random.random_sample(len(classes))
-    for row in data:
-        input_vector = create_char_count_list(row[1])
-        input_vector = normalize_vector(input_vector)
-        d = row[0]
-        y = perceptron(classes[d], theta[theta_mapper[d]], input_vector)
+    for _ in range(3):
+        classes = {row: np.random.random_sample(26) for row in np.unique(data[:, 0])}
+        theta_mapper = {list(classes.keys())[i]: i for i in range(len(classes))}
+        theta = np.random.random_sample(len(classes))
+        for i in range(1, 5001):
+            for row in data:
+                d, y, input_vector = train_layer(row, classes, theta_mapper, theta)
+                classes, theta = modify_params(classes, theta_mapper, theta, alpha, d, y, input_vector)
+            test_layer(i, classes, theta_mapper, theta, test_data, data, alpha)
+        alpha /= 10
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
